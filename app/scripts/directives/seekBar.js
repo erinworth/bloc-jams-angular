f3f886149583a113fa563f9f50e23c7e9dd75883
erinworth <eeworth@gmail.com>
2017-07-04 16:29:40 -0700

    Angular Checkpoint 10: Completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -13,13 +13,23 @@ return {
     templateUrl: '/templates/directives/seek_bar.html',
     replace: true,
     restrict: 'E',
-    scope: { },
+    scope: {
+     onChange: '&'
+    },
     link: function(scope, element, attributes) {
     // directive logic to return
         scope.value = 0;
         scope.max = 100;
 
         var seekBar = $(element);
+
+        attributes.$observe('value', function(newValue) {
+            scope.value = newValue;
+        });
+
+        attributes.$observe('max', function(newValue) {
+            scope.max = newValue;
+        });
 
     var percentString = function () {
         var value = scope.value;
@@ -39,6 +49,7 @@ return {
     scope.onClickSeekBar = function(event) {
         var percent = calculatePercent(seekBar, event);
         scope.value = percent * scope.max;
+        notifyOnChange(scope.value);
     };
 
     scope.trackThumb = function() {
@@ -46,6 +57,7 @@ return {
           var percent = calculatePercent(seekBar, event);
           scope.$apply(function() {
              scope.value = percent * scope.max;
+             notifyOnChange(scope.value);
         });
     });
 
@@ -54,7 +66,12 @@ return {
             $document.unbind('mouseup.thumb');
     });
 };
-    }
+  var notifyOnChange = function(newValue) {
+      if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+      }
+  };
+  }
   };
 }
 
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,6 @@
 
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
 
  /**
@@ -33,6 +33,12 @@
                   preload: true
               });
 
+              currentBuzzObject.bind('timeupdate', function() {
+                  $rootScope.$apply(function() {
+                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                  });
+              });
+
               SongPlayer.currentSong = song;
          };
 
@@ -51,6 +57,12 @@
 * @type {Object}
 */
        SongPlayer.currentSong = null;
+
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+      SongPlayer.currentTime = null;
 
 
 /**
@@ -143,10 +155,21 @@
               }
         };
 
+/**
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+      SongPlayer.setCurrentTime = function(time) {
+          if (currentBuzzObject) {
+              currentBuzzObject.setTime(time);
+          }
+      };
+
     return SongPlayer;
 }
 
 angular
     .module('blocJams')
-    .factory('SongPlayer', ['Fixtures', SongPlayer]);
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
